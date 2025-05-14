3aebf71a7649e04a75ef916de69f327eb5db4bb7
Esteban Guzzo <estebanguzzo@gmail.com>
2025-05-14 10:56:48 -0300

    Fix mongoose conection

--- src/config/dbConfig.mjs
+++ src/config/dbConfig.mjs
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://estebanguzzo:WUxXObcf41DR5630@sprint2tp3.h7xzvqf.mongodb.net/');
+        await mongoose.connect('mongodb+srv://Grupo-11:grupo11@cursadanodejs.ls9ii.mongodb.net/Node-js');
         console.log('Conexión exitosa a MongoDB');
     } catch (error) {
         console.error('Error al conectar a MongoDB:', error);
